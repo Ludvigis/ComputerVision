4069b19f0b8530256711f607aa4a8461f30d829a
Ludvigis <Ludvig.isaksson@gmail.com>
2018-03-23 09:03:03 +0100

    img display

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,38 @@
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
+import org.opencv.core.*;
+import org.opencv.imgcodecs.*;
+import org.opencv.imgproc.*;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
 
 public class Main {
-
+	//TODO Maven or Gradle ...
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Mat asd = new Mat(2);
+		Mat img = Imgcodecs.imread("res/images/shapes.gif");
+		displayImage(img);
+		
+	}
+	
+	public static void displayImage(Mat img) {
+	    Imgproc.resize(img, img, new Size(640, 480));
+	    MatOfByte matOfByte = new MatOfByte();
+	    Imgcodecs.imencode(".jpg", img, matOfByte);
+	    byte[] byteArray = matOfByte.toArray();
+	    BufferedImage bufImage = null;
+	    try {
+	        InputStream in = new ByteArrayInputStream(byteArray);
+	        bufImage = ImageIO.read(in);
+	        JFrame frame = new JFrame();
+	        frame.getContentPane().add(new JLabel(new ImageIcon(bufImage)));
+	        frame.pack();
+	        frame.setVisible(true);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
 	}
 
 }
