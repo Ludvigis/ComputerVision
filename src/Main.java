6382c1a1a201b1956d5db9f201bc99a8097df9c6
Ludvigis <Ludvig.isaksson@gmail.com>
2018-03-23 08:47:44 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Mat asd = new Mat(2);
 	}
 
 }
