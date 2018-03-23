2f7bceab01872cb06740d065cd985a8550851df3
Ludvigis <Ludvig.isaksson@gmail.com>
2018-03-23 13:50:15 +0100

    added printouts for shape

--- src/FeatureExtraction.java
+++ src/FeatureExtraction.java
@@ -20,15 +20,38 @@ public class FeatureExtraction {
 		Imgproc.Canny(img, edges, 100, 300);
 		Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
 		
-		//third arg negative = draw all contours...
-		Imgproc.drawContours(img, contours, -1, new Scalar(0,0,0),5);
+		//third argument negative = draw all contours...
+		Imgproc.drawContours(img, contours, -1, new Scalar(255,0,0),5);
+			
 		
-		
-		//MatOfPoint2f mop = new MatOfPoint2f(edges);
-		
-		//Imgproc.approxPolyDP(mop, (MatOfPoint2f) result, 0.04* Imgproc.arcLength(mop, true), true);
-		
-		return img;
+		MatOfPoint2f approxCurve = new MatOfPoint2f();
+		MatOfPoint2f curve = new MatOfPoint2f();
+		for (int i = 0; i < contours.size(); i++) {
+			Rect rect = Imgproc.boundingRect(contours.get(i));
+			int centerX = rect.x + rect.width/2;
+			int centerY = rect.y + rect.height/2;
+			
+			//marks center
+			Imgproc.circle(edges,new Point(centerX,centerY),8,new Scalar(0,0,0),-1);
+			Imgproc.circle(edges,new Point(centerX,centerY),4,new Scalar(255,255,255),-1);
+			
+			//writes out contour integer.
+			Imgproc.putText(edges, Integer.toString(i), new Point(centerX + 10,centerY), Core.FONT_HERSHEY_SIMPLEX, 1, new Scalar(255,100,255));
+			
+			MatOfPoint c = contours.get(i);
+			
+			curve.fromList(c.toList());
+			Imgproc.approxPolyDP(curve, approxCurve, 0.01* Imgproc.arcLength(curve, true), true);
+			if(approxCurve.total() == 3) {
+				System.out.println(i + ": " + " Triangel");
+			}else if(approxCurve.total() == 4) {
+				System.out.println(i + ": " + " Fyrkant");
+			}else if(approxCurve.total() > 8) {
+				System.out.println(i + ": " + " Cirkel");
+			}
+			
+		}
+		return edges;
 	}
 	
 }
